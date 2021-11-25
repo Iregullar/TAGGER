a8c296a299f0a8524990d4ce9db90a5e22854090
Irregullar <89784677+Iregullar@users.noreply.github.com>
2021-11-26 00:37:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,24 +25,22 @@ async def cancel(event):
 
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
-  await event.reply("**ᴛᴀɢɢᴇʀʙᴏᴛꜱ**, ɢʀᴜᴘ ᴠᴇʏᴀ ᴋᴀɴᴀʟᴅᴀᴋɪ ɴᴇʀᴇᴅᴇʏꜱᴇ ᴛᴜᴍ ᴜʏᴇʟᴇʀᴅᴇɴ ʙᴀʜꜱᴇᴅᴇʙɪʟɪʀɪᴍ ʙᴜ ᴛᴜʀ ʙᴏᴛʟᴀʀ ɪᴄɪɴ ᴋᴏᴅ ꜱᴀʜɪʙɪ ɪʟᴇ ɪʟᴇᴛɪꜱɪᴍᴇ ɢᴇᴄɪɴ @jackdanielssx ★\nᴅᴀʜᴀ ꜰᴀᴢʟᴀ ʙɪʟɢɪ ɪᴄɪɴ **/help**'i ᴛɪᴋʟᴀʏɪɴ.",
+  await event.reply("**𝐄𝐱𝐭𝐫𝐚𝐓𝐚𝐆𝐆𝐞𝐑**, 𝙶𝚛𝚞𝚙 𝚟𝚎𝚢𝚊 𝚔𝚊𝚗𝚊𝚕𝚍𝚊𝚔𝚒 𝚗𝚎𝚛𝚎𝚍𝚎𝚢𝚜𝚎 𝚝ü𝚖 ü𝚢𝚎𝚕𝚎𝚛𝚍𝚎𝚗 𝚋𝚊𝚑𝚜𝚎𝚍𝚎𝚋𝚒𝚕𝚒𝚛𝚒𝚖 ★\𝚗𝙳𝚊𝚑𝚊 𝚏𝚊𝚣𝚕𝚊 𝚋𝚒𝚕𝚐𝚒 𝚒ç𝚒𝚗 **/𝚑𝚎𝚕𝚙**'𝚒 𝚝ı𝚔𝚕𝚊𝚢ı𝚗.",
                     buttons=(
-                      [Button.url('🌟 ʙᴇɴɪ ʙɪʀ ɢʀᴜʙᴀ ᴇᴋʟᴇ', 'https://t.me/jacktaggerbot?startgroup=a'),
-                      Button.url('📣 ꜱᴜᴘᴘᴏʀᴛ', 'https://t.me/jackdanielssx'),
-                      Button.url('💻 ᴄʀᴇᴀᴛᴏʀ', 'https://t.me/jackdanielssx'),
-                      Button.url('🚀 ꜱᴀʜɪʙɪᴍ', 'https://t.me/jackdanielssx')]
+                      [Button.url('🌟 𝙱𝚎𝚗𝚒 𝙱𝚒𝚛 𝙶𝚛𝚞𝚋𝚊 𝙴𝚔𝚕𝚎', 'https://t.me/ExtraTaGGerbot?startgroup=a'),
+                      Button.url('📣 𝙺𝚊𝚗𝚊𝚕', 'https://t.me/Extremekanallar'),
+                      Button.url('🚀 𝚂𝚊𝚑𝚒𝚋𝚒𝚖', 'https://t.me/OrmanCocuklariylaMucadele')]
                     ),
                     link_preview=False
                    )
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
-  helptext = "**ᴜꜱᴇʀᴛᴀɢɢᴇʀ ʙᴏᴛ'ᴜɴ ʏᴀʀᴅɪᴍ ᴍᴇɴᴜꜱᴜ**\n\nKomut: /all \n  ʙᴜ ᴋᴏᴍᴜᴛᴜ, ʙᴀꜱᴋᴀʟᴀʀɪɴᴀ ʙᴀʜꜱᴇᴛᴍᴇᴋ ɪꜱᴛᴇᴅɪɢɪɴɪᴢ ᴍᴇᴛɪɴʟᴇ ʙɪʀʟɪᴋᴛᴇ ᴋᴜʟʟᴀɴᴀʙɪʟɪʀꜱɪɴɪᴢ. \n`ᴏʀɴᴇᴋ: /all Günaydın!`  \nʙᴜ ᴋᴏᴍᴜᴛᴜ ʏᴀɴɪᴛ ᴏʟᴀʀᴀᴋ ᴋᴜʟʟᴀɴᴀʙɪʟɪʀꜱɪɴɪᴢ. ʜᴇʀʜᴀɴɢɪ ʙɪʀ ᴍᴇꜱᴀᴊ ʙᴏᴛ, ʏᴀɴɪᴛʟᴀɴᴀɴ ɪʟᴇᴛɪʏᴇ ᴋᴜʟʟᴀɴɪᴄɪʟᴀʀɪ ᴇᴛɪᴋᴇᴛʟᴇʏᴇᴄᴇᴋ"
+  helptext = "**𝐄𝐱𝐭𝐫𝐚𝐓𝐚𝐆𝐆𝐞𝐑 𝚋𝚘𝚝'𝚞𝚗 𝚈𝚊𝚛𝚍ı𝚖 𝙼𝚎𝚗ü𝚜ü**\𝚗\𝚗𝙺𝚘𝚖𝚞𝚝: /𝚊𝚕𝚕 \𝚗 𝙱𝚞 𝚔𝚘𝚖𝚞𝚝𝚞, 𝚋𝚊ş𝚔𝚊𝚕𝚊𝚛ı𝚗𝚊 𝚋𝚊𝚑𝚜𝚎𝚝𝚖𝚎𝚔 𝚒𝚜𝚝𝚎𝚍𝚒ğ𝚒𝚗𝚒𝚣 𝚖𝚎𝚝𝚒𝚗𝚕𝚎 𝚋𝚒𝚛𝚕𝚒𝚔𝚝𝚎 𝚔𝚞𝚕𝚕𝚊𝚗𝚊𝚋𝚒𝚕𝚒𝚛𝚜𝚒𝚗𝚒𝚣. \𝚗`Ö𝚛𝚗𝚎𝚔: /𝚊𝚕𝚕 𝙶ü𝚗𝚊𝚢𝚍ı𝚗!` \𝚗𝙱𝚞 𝚔𝚘𝚖𝚞𝚝𝚞 𝚢𝚊𝚗ı𝚝 𝚘𝚕𝚊𝚛𝚊𝚔 𝚔𝚞𝚕𝚕𝚊𝚗𝚊𝚋𝚒𝚕𝚒𝚛𝚜𝚒𝚗𝚒𝚣. 𝚑𝚎𝚛𝚑𝚊𝚗𝚐𝚒 𝚋𝚒𝚛 𝚖𝚎𝚜𝚊𝚓 𝙱𝚘𝚝, 𝚢𝚊𝚗ı𝚝𝚕𝚊𝚗𝚊𝚗 𝚒𝚕𝚎𝚝𝚒𝚢𝚎 𝚔𝚞𝚕𝚕𝚊𝚗ı𝚌ı𝚕𝚊𝚛ı 𝚎𝚝𝚒𝚔𝚎𝚝𝚕𝚎𝚢𝚎𝚌𝚎𝚔"
   await event.reply(helptext,
                     buttons=(
-                      [Button.url('🌟 ʙᴇɴɪ ʙɪʀ ɢʀᴜʙᴀ ᴇᴋʟᴇ', 'https://t.me/jacktaggerbot?startgroup=a'),
-                       Button.url('📣 ꜱᴜᴘᴘᴏʀᴛ', 'https://t.me/jackdanielssx'),
-                       Button.url('💻 ᴄʀᴇᴀᴛᴏʀ', 'https://t.me/jackdanielssx'),
-                      Button.url('🚀 ꜱᴀʜɪʙɪᴍ', 'https://t.me/jackdanielssx')]
+                      [Button.url('🌟 𝙱𝚎𝚗𝚒 𝙱𝚒𝚛 𝙶𝚛𝚞𝚋𝚊 𝙴𝚔𝚕𝚎', 'https://t.me/ExtraTaGGerbot?startgroup=a'),
+                      Button.url('📣 𝙺𝚊𝚗𝚊𝚕', 'https://t.me/Extremekanallar'),
+                      Button.url('🚀 𝚂𝚊𝚑𝚒𝚋𝚒𝚖', 'https://t.me/OrmanCocuklariylaMucadele')]
                     ),
                     link_preview=False
                    )
@@ -52,13 +50,13 @@ async def help(event):
 async def mentionall(event):
   global anlik_calisan
   if event.is_private:
-    return await event.respond("__ʙᴜ ᴋᴏᴍᴜᴛ ɢʀᴜᴘʟᴀʀᴅᴀ ᴠᴇ ᴋᴀɴᴀʟʟᴀʀᴅᴀ ᴋᴜʟʟᴀɴɪʟᴀʙɪʟɪʀ.!__")
+    return await event.respond("__𝙱𝚞 𝚔𝚘𝚖𝚞𝚝 𝚐𝚛𝚞𝚙𝚕𝚊𝚛𝚍𝚊 𝚟𝚎 𝚔𝚊𝚗𝚊𝚕𝚕𝚊𝚛𝚍𝚊 𝚔𝚞𝚕𝚕𝚊𝚗ı𝚕𝚊𝚋𝚒𝚕𝚒𝚛.!__")
   
   admins = []
   async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
     admins.append(admin.id)
   if not event.sender_id in admins:
-    return await event.respond("__ʏᴀʟɴɪᴢᴄᴀ ʏᴏɴᴇᴛɪᴄɪʟᴇʀ ʜᴇᴘꜱɪɴᴅᴇɴ ʙᴀʜꜱᴇᴅᴇʙɪʟɪʀ!__")
+    return await event.respond("__𝚈𝚊𝚕𝚗ı𝚣𝚌𝚊 𝚢ö𝚗𝚎𝚝𝚒𝚌𝚒𝚕𝚎𝚛 𝚑𝚎𝚙𝚜𝚒𝚗𝚍𝚎𝚗 𝚋𝚊𝚑𝚜𝚎𝚍𝚎𝚋𝚒𝚕𝚒𝚛!__")
   
   if event.pattern_match.group(1):
     mode = "text_on_cmd"
@@ -67,11 +65,11 @@ async def mentionall(event):
     mode = "text_on_reply"
     msg = event.reply_to_msg_id
     if msg == None:
-        return await event.respond("__ᴇꜱᴋɪ ᴍᴇꜱᴀᴊʟᴀʀ ɪᴄɪɴ ᴜʏᴇʟᴇʀᴅᴇɴ ʙᴀʜꜱᴇᴅᴇᴍᴇᴍ! (ɢʀᴜʙᴀ ᴇᴋʟᴇᴍᴇᴅᴇɴ ᴏɴᴄᴇ ɢᴏɴᴅᴇʀɪʟᴇɴ ᴍᴇꜱᴀᴊʟᴀʀ)__")
+        return await event.respond("__𝙴𝚜𝚔𝚒 𝚖𝚎𝚜𝚊𝚓𝚕𝚊𝚛 𝚒ç𝚒𝚗 ü𝚢𝚎𝚕𝚎𝚛𝚍𝚎𝚗 𝚋𝚊𝚑𝚜𝚎𝚍𝚎𝚖𝚎𝚖! (𝚐𝚛𝚞𝚋𝚊 𝚎𝚔𝚕𝚎𝚖𝚎𝚍𝚎𝚗 ö𝚗𝚌𝚎 𝚐ö𝚗𝚍𝚎𝚛𝚒𝚕𝚎𝚗 𝚖𝚎𝚜𝚊𝚓𝚕𝚊𝚛)__")
   elif event.pattern_match.group(1) and event.reply_to_msg_id:
-    return await event.respond("__ʙᴀɴᴀ ʙɪʀ ᴀʀɢᴜᴍᴀɴ ᴠᴇʀ!__")
+    return await event.respond("__𝙱𝚊𝚗𝚊 𝚋𝚒𝚛 𝚊𝚛𝚐ü𝚖𝚊𝚗 𝚟𝚎𝚛!__")
   else:
-    return await event.respond("__ʙɪʀ ᴍᴇꜱᴀᴊɪ ʏᴀɴɪᴛʟᴀʏɪɴ ᴠᴇʏᴀ ʙᴀꜱᴋᴀʟᴀʀɪɴᴅᴀɴ ʙᴀʜꜱᴇᴛᴍᴇᴍ ɪᴄɪɴ ʙᴀɴᴀ ʙɪʀ ᴍᴇᴛɪɴ ᴠᴇʀɪɴ!__")
+    return await event.respond("__𝙱𝚒𝚛 𝚖𝚎𝚜𝚊𝚓ı 𝚢𝚊𝚗ı𝚝𝚕𝚊𝚢ı𝚗 𝚟𝚎𝚢𝚊 𝚋𝚊ş𝚔𝚊𝚕𝚊𝚛ı𝚗𝚍𝚊𝚗 𝚋𝚊𝚑𝚜𝚎𝚝𝚖𝚎𝚖 𝚒ç𝚒𝚗 𝚋𝚊𝚗𝚊 𝚋𝚒𝚛 𝚖𝚎𝚝𝚒𝚗 𝚟𝚎𝚛𝚒𝚗!__")
     
   if mode == "text_on_cmd":
     anlik_calisan.append(event.chat_id)
@@ -81,7 +79,7 @@ async def mentionall(event):
       usrnum += 1
       usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
       if event.chat_id not in anlik_calisan:
-        await event.respond("ɪꜱʟᴇᴍ ʙᴀꜱᴀʀɪʟɪ ʙɪʀ ꜱᴇᴋɪʟᴅᴇ ᴅᴜʀᴅᴜʀᴜʟᴅᴜ ❌")
+        await event.respond("İş𝚕𝚎𝚖 𝙱𝚊ş𝚊𝚛ı𝚕ı 𝙱𝚒𝚛 Ş𝚎𝚔𝚒𝚕𝚍𝚎 𝙳𝚞𝚛𝚍𝚞𝚛𝚞𝚕𝚍𝚞 ❌")
         return
       if usrnum == 5:
         await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
@@ -99,7 +97,7 @@ async def mentionall(event):
       usrnum += 1
       usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
       if event.chat_id not in anlik_calisan:
-        await event.respond("ɪꜱʟᴇᴍ ʙᴀꜱᴀʀɪʟɪ ʙɪʀ ꜱᴇᴋɪʟᴅᴇ ᴅᴜʀᴅᴜʀᴜʟᴅᴜ ❌")
+        await event.respond("İş𝚕𝚎𝚖 𝙱𝚊ş𝚊𝚛ı𝚕ı 𝙱𝚒𝚛 Ş𝚎𝚔𝚒𝚕𝚍𝚎 𝙳𝚞𝚛𝚍𝚞𝚛𝚞𝚕𝚍𝚞 ❌")
         return
       if usrnum == 5:
         await client.send_message(event.chat_id, usrtxt, reply_to=msg)
@@ -108,5 +106,5 @@ async def mentionall(event):
         usrtxt = ""
 
 
-print(">> ʙᴏᴛ ᴄᴀʟɪʏᴏʀ ᴍᴇʀᴀᴋ ᴇᴛᴍᴇ 🚀 @jackdanielssx ʙɪʟɢɪ ᴀʟᴀʙɪʟɪʀꜱɪɴ <<")
+print(">> 𝙱𝚘𝚝 ç𝚊𝚕ı𝚢𝚘𝚛 𝚖𝚎𝚛𝚊𝚔 𝚎𝚝𝚖𝚎 🚀 @OrmanCocuklariylaMucadele 𝚋𝚒𝚕𝚐𝚒 𝚊𝚕𝚊𝚋𝚒𝚕𝚒𝚛𝚜𝚒𝚗 <<")
 client.run_until_disconnected()
